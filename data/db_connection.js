161775e26c826f14e175b1c623d6fafd311c839e
lostmart <martinpedraza1979@gmail.com>
2024-07-25 12:15:55 +0200

    new data validation

--- data/db_connection.js
+++ data/db_connection.js
@@ -1,23 +1,18 @@
 //DB connection
 const sqlite3 = require("sqlite3").verbose()
-let db = new sqlite3.Database(
-	"./data/dbFile.db",
-	sqlite3.OPEN_READWRITE,
-	(err) => {
-		if (err) {
-			return console.error(err.message)
-		}
-		console.log("Connected to the dbFile.db SQlite database.")
+let db = new sqlite3.Database("./dbFile.db", sqlite3.OPEN_READWRITE, (err) => {
+	if (err) {
+		return console.error(err.message)
 	}
-)
+	console.log("Connected to the SQlite database.")
+})
 
 // DB CLOSE
 db.close((err) => {
 	if (err) {
 		return console.error(err.message)
 	}
-	console.log("Close the database connection.")
+	console.log("Database connection ended...")
 })
-
 
 module.exports = db
--- data/queries.js
+++ data/queries.js
@@ -1,12 +1,45 @@
+const Joi = require("joi")
 const sqlite3 = require("sqlite3").verbose()
 let sql
 
-// connect to DB
-let db = new sqlite3.Database(
-	"./mydatabase.db",
-	sqlite3.OPEN_READWRITE,
-	(err) => handleError(err, "all good and connected")
-)
+const User = require("../classes/User")
+
+// const db = require("./db_connection")
+
+// new user using User class model
+try {
+	newUser = new User("bob", "baby@baby.net", "123@", 2004)
+	if (!newUser) new Error("error creating user with the class", error)
+	console.log(newUser)
+} catch (error) {
+	console.log("no no. no User created ...", error)
+}
+
+// Joi validation
+const minDate = new Date().getFullYear() - 18
+const userSchema = Joi.object({
+	userName: Joi.string().min(3).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string()
+		.pattern(new RegExp("^[a-zA-Z0-9@#$%&]{3,30}$"))
+		.required(),
+	birthYear: Joi.number().integer().min(1924).max(minDate).required(),
+})
+
+const validateUser = (user) => {
+	const { error } = userSchema.validate(user)
+	if (error) {
+		throw new Error(`Validation error: ${error.details[0].message}`)
+	}
+}
+
+try {
+	validateUser(newUser)
+	// Insert into SQLite database
+	console.log("now you can run the stuff")
+} catch (err) {
+	console.error(err.message)
+}
 
 // *** create table  ***
 // sql = `CREATE TABLE Users (
@@ -37,17 +70,17 @@ let db = new sqlite3.Database(
 
 // *** delete data  ***
 // check it exists first
-sql = sql = "SELECT * FROM users WHERE id = ?"
-db.all(sql, [5], (err, rows) => {
-	if (err) return console.log(err.message)
-	if (!rows.length) return console.log("no user with that id found ...")
+// sql = sql = "SELECT * FROM users WHERE id = ?"
+// db.all(sql, [5], (err, rows) => {
+// 	if (err) return console.log(err.message)
+// 	if (!rows.length) return console.log("no user with that id found ...")
 
-	sql = "DELETE from users WHERE id = ?"
-	db.run(sql, [5], (err) => {
-		if (err) return console.log(err.message)
-		console.log("user deleted !!! :o ")
-	})
-})
+// 	sql = "DELETE from users WHERE id = ?"
+// 	db.run(sql, [5], (err) => {
+// 		if (err) return console.log(err.message)
+// 		console.log("user deleted !!! :o ")
+// 	})
+// })
 
 // *** query the db  ***
 // sql = "SELECT * FROM users"
@@ -59,7 +92,7 @@ db.all(sql, [5], (err, rows) => {
 // })
 
 // *** close the database connection  ***
-db.close()
+// db.close()
 
 function handleError(err, successMsg) {
 	if (err) return console.log(err.message)
