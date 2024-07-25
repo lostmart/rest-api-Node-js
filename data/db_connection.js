8dcc7ed01e7203278b3528f8bae185ab95a6ec0f
lostmart <martinpedraza1979@gmail.com>
2024-07-25 12:41:33 +0200

    db connection handler

--- data/db_connection.js
+++ data/db_connection.js
@@ -1,18 +1,22 @@
 //DB connection
 const sqlite3 = require("sqlite3").verbose()
-let db = new sqlite3.Database("./dbFile.db", sqlite3.OPEN_READWRITE, (err) => {
-	if (err) {
-		return console.error(err.message)
+let db = new sqlite3.Database(
+	"./data/db_file.db",
+	sqlite3.OPEN_READWRITE,
+	(err) => {
+		if (err) {
+			return console.error(err.message)
+		}
+		console.log("Connected to the database.")
 	}
-	console.log("Connected to the SQlite database.")
-})
+)
 
 // DB CLOSE
-db.close((err) => {
-	if (err) {
-		return console.error(err.message)
-	}
-	console.log("Database connection ended...")
-})
+// db.close((err) => {
+// 	if (err) {
+// 		return console.error(err.message)
+// 	}
+// 	console.log("Database connection ended...")
+// })
 
 module.exports = db
