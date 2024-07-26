4ec5cf6546b3d689289e7344d8f37a1d6da40c4d
lostmart <martinpedraza1979@gmail.com>
2024-07-26 10:06:09 +0200

    new control

---
+++ controllers/user.js
@@ -0,0 +1,119 @@
+const util = require("util")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const ClassUser = require("../classes/User")
+const validateUser = require("../models/user")
+
+// db connection
+const db = require("../data/db_connection")
+// Inisialization of sql
+let sql = null
+
+require("dotenv").config()
+
+exports.signup = async (req, res) => {
+	const { userName, email, password, birthYear } = req.body
+	// new User with class User
+	const newUser = new ClassUser(userName, email, password, birthYear)
+
+	// user Schema validation
+	try {
+		validateUser(newUser)
+		// Insert into SQLite database
+	} catch (err) {
+		console.error(err.message)
+		return res.status(400).json({ msg: err.message })
+	}
+
+	// save data into DB
+	try {
+		// hash password
+		newUser.password = await bcrypt.hash(req.body.password, 10)
+		// prepare ayyay from user Object
+		const valuesArray = Object.values(newUser)
+
+		// run query to insert data into DB
+		sql =
+			"INSERT INTO users(userName, email, password, birthYear) VALUES(?,?,?,?)"
+
+		db.run(sql, valuesArray, (err) => {
+			if (err) {
+				new Error("Error saving new user ...")
+				return res.status(400).json({ msg: err.message })
+			}
+			// get created user from DB
+			sql = "SELECT * FROM users WHERE email = ? "
+			let createdUser = {}
+			db.all(sql, [email], (err, rows) => {
+				if (err) return console.log(err.message)
+
+				if (rows.length) {
+					rows.forEach((row) => {
+						createdUser.userName = row.userName
+						createdUser.email = row.email
+						createdUser.birthYear = row.birthYear
+						createdUser.userId = row.id
+					})
+				} else {
+					return res.status(500).json({
+						msg: "Something went wrong... please try again",
+					})
+				}
+				return res.status(201).json({
+					msg: "User created",
+					user: createdUser,
+				})
+			})
+		})
+	} catch (error) {
+		return res.status(500).json({ error })
+	}
+}
+
+exports.login = async (req, res) => {
+	const { email, password } = req.body
+
+	let foundUser = {}
+	const sql = "SELECT * FROM users WHERE email = ? "
+
+	try {
+		// Promisify db.all -> creates a promise from the callback fn of SQLite3
+		const dbAll = util.promisify(db.all).bind(db)
+
+		const rows = await dbAll(sql, [email])
+
+		// no user found ...
+		if (!rows.length) {
+			return res.status(400).json({ msg: "Wrong credentials ..." })
+		} else {
+			rows.forEach((row) => {
+				foundUser.password = row.password
+				foundUser.email = row.email
+			})
+			// console.log(email, password, foundUser.password)
+		}
+
+		const valid = await bcrypt.compare(password, foundUser.password)
+
+		if (valid) {
+			const token = jwt.sign(
+				{
+					email,
+				},
+				process.env.RANDOM_SECRET_WORD,
+				{ expiresIn: "24h" }
+			)
+			return res.status(200).json({ token })
+		} else {
+			return res.status(400).json({ msg: "Wrong credentials ..." })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ msg: "An error occurred." })
+	}
+}
+
+
+exports.getAllUsers = async (req, res) => {
+	
+}
