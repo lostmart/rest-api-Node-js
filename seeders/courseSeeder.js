ad49397d8f875ef59854c75ec88433907c5dbdeb
lostmart <martinpedraza1979@gmail.com>
2024-07-30 10:18:54 +0200

    seeders and jsondata aded

--- seeders/courseSeeder.js
+++ seeders/courseSeeder.js
@@ -1,20 +1,41 @@
 // /seeders/userSeeder.js
-import sqlite3 from require("sqlite3")
-    sqlite3.verbose()
-const db = new sqlite3.Database("./example.db")
+import courses from "./seedData/coursesSeedData.json" assert { type: "json" }
+import sqlite3 from "sqlite3"
+sqlite3.verbose()
+const db = new sqlite3.Database(
+	"../data/db_file.db",
+	sqlite3.OPEN_READWRITE,
+	(err) => {
+		if (err) return console.log(err.message)
+		console.log("you're in ")
+	}
+)
 
-const seedUsers = () => {
+export const seedUsers = () => {
 	db.serialize(() => {
-		db.run(`CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE
-        )`)
+		db.run(`CREATE TABLE IF NOT EXISTS courses (
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    title TEXT NOT NULL,
+		    description TEXT NOT NULL,
+			credits INTEGER NOT NULL,
+			created_at TEXT NOT NULL,
+			updated_at TEXT NOT NULL
 
-		const insert = db.prepare("INSERT INTO users (name, email) VALUES (?, ?)")
-		insert.run("John Doe", "john.doe@example.com")
-		insert.run("Jane Smith", "jane.smith@example.com")
-		insert.run("Alice Johnson", "alice.johnson@example.com")
+		)`)
+		const insert = db.prepare(
+			`INSERT INTO
+				courses (title, description, credits, created_at, updated_at) 
+				VALUES (?, ?, ?, ?, ?)`
+		)
+		courses.forEach((course) => {
+			insert.run(
+				course.title,
+				course.title,
+				course.credits,
+				course.created_at,
+				course.updated_at
+			)
+		})
 		insert.finalize()
 	})
 
@@ -26,4 +47,11 @@ const seedUsers = () => {
 	})
 }
 
-module.exports = seedUsers
+// *** drop a table ***
+// const sql = "DROP TABLE courses"
+// db.run(sql, (err) => {
+// 	if (err) return console.log(err.message)
+// 	console.log("table deleted ...")
+// })
+
+seedUsers()
