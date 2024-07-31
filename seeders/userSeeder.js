a5b4f3e8b6c3fe04de3dd74b24fc4846d7f25058
lostmart <martinpedraza1979@gmail.com>
2024-07-31 15:04:18 +0200

    new dull users aded and sedders

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -1,3 +1,4 @@
+import users from "./seedData/usersSeedData.json" assert { type: "json" }
 import sqlite3 from "sqlite3"
 sqlite3.verbose()
 const db = new sqlite3.Database(
@@ -9,8 +10,41 @@ const db = new sqlite3.Database(
 	}
 )
 
-export const seedCourses = () => {
-	db.serialize(() => {})
+export const seedUsers = () => {
+	db.serialize(() => {
+		db.run(`CREATE TABLE IF NOT EXISTS users (
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    userName TEXT NOT NULL,
+			userImg TEXT NOT NULL,		
+		    email TEXT UNIQUE NOT NULL,
+			birthYear INTEGER NOT NULL,
+			created_at TEXT NOT NULL,
+			updated_at TEXT NOT NULL,
+			contacts TEXT,
+			enrolled TEXT
+		)`)
+		const insert = db.prepare(
+			`INSERT INTO
+				users (userName, userImg, email, birthYear, created_at, updated_at, contacts, enrolled) 
+				VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+		)
+
+		users.forEach((user) => {
+			const externalImg = `https://api.dicebear.com/9.x/adventurer/svg?seed=${user.userName}`
+			insert.run(
+				user.userName,
+				externalImg,
+				user.email,
+				user.birthYear,
+				user.created_at,
+				user.updated_at,
+				"",
+				""
+			)
+		})
+
+		insert.finalize()
+	})
 	db.close((err) => {
 		if (err) {
 			console.error(err.message)
@@ -19,8 +53,11 @@ export const seedCourses = () => {
 	})
 }
 
-// *** drop a table ***
-// sql = "DROP TABLE users"
-// db.run(sql, (err) => handleError(err, "table dropped"))
+// *** ❗ drop a table ❗ ***
+const sql = "DROP TABLE users"
+// db.run(sql, (err) => {
+// 	if (err) return console.log(err.message)
+// 	console.log("users table deleted ...")
+// })
 
-seedCourses()
+seedUsers()
